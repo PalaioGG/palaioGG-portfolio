c67153b272d76f6c644eb868f0d4944058b82c9e
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-09-21 17:39:26 +0300

    Update Controller

--- palaioGG/js/mainController.js
+++ palaioGG/js/mainController.js
@@ -1,35 +1,17 @@
-﻿// Code goes here
-var myApp = angular.module('myApp', ['ng-peity']);
-myApp.controller('myAppController', ['$scope', '$interval', function ($scope, $interval) {
-    $scope.name = 'ng-peity';
-    $scope.BarChart = {
-        data: [1, 2, 3, 4, 3, 2, 1],
-        options: {
-            width: '200',
-            height: 40,
-            fill: ['#f00']
-        }
-    };
-    $scope.PieChart = {
-        data: [30, 30, 25],
-        options: {
-            width: 75,
-            height: 75,
-            innerRadius: 20
-        }
-    };
+﻿var myApp = angular.module('palaioApp', ['gridshore.c3js.chart']);
 
-    $scope.LineChart = {
-        data: [1, 2, 3, 4, 3, 2, 1, 9, 7, 5, 4, 12, 4, 9],
-        options: {
-            width: 275,
-            height: 75
-        }
-    };
-    $interval(function () {
-        var random = Math.round(Math.random() * 10);
-        $scope.BarChart.data.shift();
-        $scope.BarChart.data.push(random);
-        $scope.BarChart.options.fill[0] = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    }, 1000);
-}]);
+myApp.controller('palaioController', [
+    '$scope',
+    function ($scope) {
+        $scope.spice = 'very';
+
+        $scope.chiliSpicy = function () {
+            $scope.spice = 'chili';
+        };
+
+        $scope.jalapenoSpicy = function () {
+            $scope.spice = 'jalapeño';
+        };
+    }
+]);
+
